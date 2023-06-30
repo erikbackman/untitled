d71d7f4fc96ca5d2dcc36111fb88841de7a0f8e6
erikbackman <erikbackman@users.noreply.github.com>
2023-06-30 17:36:02 +0200

    cleanup

--- src/shader.glsl
+++ src/shader.glsl
@@ -1,14 +1,14 @@
 #shader vertex
 #version 330 core
-layout (location = 0) in vec3 aPos;   // the position variable has attribute position 0
-layout (location = 1) in vec3 aColor; // the color variable has attribute position 1
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aColor;
 
-out vec3 myColor; // output a color to the fragment shader
+out vec3 myColor;
 
 void main()
 {
     gl_Position = vec4(aPos, 1.0);
-    myColor = aColor; // set ourColor to the input color we got from the vertex data
+    myColor = aColor;
 }
 
 #shader fragment
