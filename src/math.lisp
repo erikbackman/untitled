a44f0fa1b3a554d959a864219c82d347d03a5304
erikbackman <erikbackman@users.noreply.github.com>
2023-07-02 17:38:09 +0200

    fix broken rotation

--- src/math.lisp
+++ src/math.lisp
@@ -19,34 +19,50 @@
 	  (incf (aref m3 r c) (* (aref m1 r i) (aref m2 i c))))))
     m3))
 
-(defun tr-scale (matrix x y z)
+(defun tr-mat4-scale (x y z)
   (let ((tr (make-identity-matrixf 4)))
     (setf
      (aref tr 0 0) x
      (aref tr 1 1) y
      (aref tr 2 2) z)
-    (matrix-mulf matrix tr)))
+    tr))
 
-(defun tr-translate (matrix x y z)
+;;           | x*x*(1-c)+c      x*y*(1-c)-z*s    x*z*(1-c)+y*s |
+;; Rotate  = | y*x*(1-c)+z*s    y*y*(1-c)+c      y*z*(1-c)-x*s |
+;;           | z*x*(1-c)-y*s    z*y*(1-c)+x*s    z*z*(1-c)+c   |
+(defun tr-mat4-rotate (angle x y z)
+  (let ((tr (make-identity-matrixf 4))
+	(c (cos angle))
+	(s (sin angle)))
+    (setf
+     (aref tr 0 0) (+ (* x x (- 1 c)) c)
+     (aref tr 0 1) (- (* x y (- 1 c)) (* z s))
+     (aref tr 0 2) (+ (* x z (- 1 c)) (* y s))
+     (aref tr 1 0) (+ (* y x (- 1 c)) (* z s))
+     (aref tr 1 1) (+ (* y y (- 1 c)) c)
+     (aref tr 1 2) (- (* y z (- 1 c)) (* x s))
+     (aref tr 2 0) (- (* z x (- 1 c)) (* y s))
+     (aref tr 2 1) (+ (* z y (- 1 c)) (* x s))
+     (aref tr 2 2) (+ (* z z (- 1 c)) c))
+    tr))
+
+(defun tr-mat4-translate (x y z)
   (let ((tr (make-identity-matrixf 4)))
     (setf
      (aref tr 0 3) x
      (aref tr 1 3) y
      (aref tr 2 3) z)
-    (matrix-mulf matrix tr)))
+    tr))
 
-(defun tr-rotate (matrix angle x y z)
-  (let ((tr (make-identity-matrixf 4))
-	(cosv (cos angle))
-	(sinv (sin angle)))
-    (setf
-     (aref tr 0 0) (+ cosv (* (expt x 2) (- 1 cosv)))
-     (aref tr 0 1) (- (* x y (- 1 cosv)) (* z sinv))
-     (aref tr 0 2) (+ (* x z (- 1 cosv)) (* y sinv))
-     (aref tr 1 0) (- (* y x (- 1 cosv)) (* z sinv))
-     (aref tr 1 1) (+ cosv (* (expt y 2) (- 1 cosv)))
-     (aref tr 1 2) (- (* y z (- 1 cosv)) (* x sinv))
-     (aref tr 2 0) (- (* z x (- 1 cosv)) (* y sinv))
-     (aref tr 2 1) (+ (* z y (- 1 cosv)) (* x sinv))
-     (aref tr 2 2) (+ cosv (* (expt z 2) (- 1 cosv))))
-    (matrix-mulf matrix tr)))
+(defun tr-scale (x y z matrix)
+  (matrix-mulf (tr-mat4-scale x y z) matrix))
+
+(defun tr-translate (x y z matrix)
+  (matrix-mulf matrix (tr-mat4-translate x y z)))
+
+(defun tr-rotate (angle x y z matrix)
+  (matrix-mulf matrix (tr-mat4-rotate angle x y z)))
+
+(defun tr-transform (matrix transformations)
+  (reduce 'funcall transformations :from-end t :initial-value matrix))
+
