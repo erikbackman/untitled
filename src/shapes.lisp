2dcf52a84cd9b4a435941bd18000f193397a7752
erikbackman <erikbackman@users.noreply.github.com>
2023-07-05 02:06:40 +0200

    add missing color elements

--- src/shapes.lisp
+++ src/shapes.lisp
@@ -21,58 +21,63 @@
   (make-shape-data
    :verts #(;; front
 	    -0.5 -0.5 +0.5 ;; 0 (bottom left)
-	    ;; +1.0 +1.0 +1.0 ;; color
+	    +1.0 +0.0 +0.0 ;; color
 	    +0.5 -0.5 +0.5 ;; 1 (bottom right)
-	    ;; +1.0 +1.0 +1.0 ;; color
+	    +1.0 +0.0 +0.0 ;; color
 	    -0.5 +0.5 +0.5 ;; 2 (top left)
-	    ;; +1.0 +1.0 +1.0 ;; color
+	    +1.0 +0.0 +0.0 ;; color
 	    +0.5 +0.5 +0.5 ;; 3 (top right)
-
+	    +1.0 +0.0 +0.0 ;; color
+	    
 	    ;; back
 	    -0.5 -0.5 -0.5 ;; 4 (bottom left)
-	    ;; +1.0 +1.0 +1.0 ;; color
+	    +1.0 +0.0 +0.0 ;; color
 	    +0.5 -0.5 -0.5 ;; 5 (bottom right)
-	    ;; +1.0 +1.0 +1.0 ;; color
+	    +1.0 +0.0 +0.0 ;; color
 	    -0.5 +0.5 -0.5 ;; 6 (top left)
-	    ;; +1.0 +1.0 +1.0 ;; color
+	    +1.0 +0.0 +0.0 ;; color
 	    +0.5 +0.5 -0.5 ;; 7 (top right)
-
+	    +1.0 +0.0 +0.0 ;; color
+	    
 	    ;; top
 	    -0.5 +0.5 +0.5 ;; 8 (top left near)
-	    ;; +1.0 +1.0 +1.0 ;; color
+	    +0.0 +1.0 +0.0 ;; color
 	    +0.5 +0.5 +0.5 ;; 9 (top right near)
-	    ;; +1.0 +1.0 +1.0 ;; color
+	    +0.0 +1.0 +0.0 ;; color
 	    -0.5 +0.5 -0.5 ;; 10 (top left far)
-	    ;; +1.0 +1.0 +1.0 ;; color
+	    +0.0 +1.0 +0.0 ;; color
 	    +0.5 +0.5 -0.5 ;; 11 (top right far)
-
+	    +0.0 +1.0 +0.0 ;; color
+	    
 	    ;; bottom
 	    -0.5 -0.5 +0.5 ;; 12 (bottom left near)
-	    ;; +1.0 +1.0 +1.0 ;; color
+	    +0.0 +1.0 +0.0 ;; color
 	    +0.5 -0.5 +0.5 ;; 13 (bottom right near)
-	    ;; +1.0 +1.0 +1.0 ;; color
+	    +0.0 +1.0 +0.0 ;; color
 	    -0.5 -0.5 -0.5 ;; 14 (bottom left far)
-	    ;; +1.0 +1.0 +1.0 ;; color
+	    +0.0 +1.0 +0.0 ;; color
 	    +0.5 -0.5 -0.5 ;; 15 (bottom right far)
-
+	    +0.0 +1.0 +0.0 ;; color
+	    
 	    ;; right
 	    +0.5 -0.5 +0.5 ;; 16 (right bottom near)
-	    ;; +1.0 +1.0 +1.0 ;; color
+	    +0.0 +0.0 +1.0 ;; color
 	    +0.5 -0.5 -0.5 ;; 17 (right bottom far)
-	    ;; +1.0 +1.0 +1.0 ;; color
+	    +0.0 +0.0 +1.0 ;; color
 	    +0.5 +0.5 +0.5 ;; 18 (right top near)
-	    ;; +1.0 +1.0 +1.0 ;; color
+	    +0.0 +0.0 +1.0 ;; color
 	    +0.5 +0.5 -0.5 ;; 19 (right top far)
-
+	    +0.0 +0.0 +1.0 ;; color
+	    
 	    ;; left
 	    -0.5 -0.5 +0.5 ;; 20 (left bottom near)
-	    ;; +1.0 +1.0 +1.0 ;; color
+	    +0.0 +0.0 +1.0 ;; color
 	    -0.5 -0.5 -0.5 ;; 21 (left bottom far)
-	    ;; +1.0 +1.0 +1.0 ;; color
+	    +0.0 +0.0 +1.0 ;; color
 	    -0.5 +0.5 +0.5 ;; 22 (left top near)
-	    ;; +1.0 +1.0 +1.0 ;; color
+	    +0.0 +0.0 +1.0 ;; color
 	    -0.5 +0.5 -0.5 ;; 23 (left top far)
-	    ;; +1.0 +1.0 +1.0 ;; color
+	    +0.0 +0.0 +1.0 ;; color
 	    )
    :vert-count 23
    :inds #(;; front
