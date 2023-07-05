872290cd7cc961cf18d543319019dc6040176efd
erikbackman <erikbackman@users.noreply.github.com>
2023-07-05 02:10:26 +0200

    update shader to use color data

--- src/shader.glsl
+++ src/shader.glsl
@@ -6,23 +6,23 @@ layout (location = 1) in vec3 a_color;
 
 uniform mat4 u_MVP;
 
-out vec3 in_color;
+out vec3 my_color;
 
 void main()
 {
     gl_Position = u_MVP * vec4(position, 1.0);
-    in_color = a_color;
+    my_color = a_color;
 }
 
 #shader fragment
 #version 330 core
 
 out vec4 frag_color;
-in vec3 in_color;
+in vec3 my_color;
 uniform vec3 u_Color;
 
 void main()
 {
-    // frag_color = vec4(in_color, 1.0);
-  frag_color = vec4(1.0, 1.0, 1.0, 1.0);
+  frag_color = vec4(my_color, 1.0);
+  //frag_color = vec4(1.0, 1.0, 1.0, 1.0);
 }
