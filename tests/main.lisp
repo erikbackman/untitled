bb0a7df630ea94011a09a032de71d303b4a9d38d
erikbackman <erikbackman@users.noreply.github.com>
2023-07-06 22:41:13 +0200

    test matrix*

--- tests/main.lisp
+++ tests/main.lisp
@@ -1,11 +1,19 @@
 (defpackage untitled/tests/main
-  (:use :cl
-        :untitled
-        :rove))
+  (:use :cl :rove)
+  (:import-from :untitled :matrix*))
+
 (in-package :untitled/tests/main)
 
-;; NOTE: To run this test file, execute `(asdf:test-system :untitled)' in your Lisp.
-
-(deftest test-target-1
-  (testing "should (= 1 1) to be true"
-    (ok (= 1 1))))
+(deftest test-matrix*
+  (testing "Should perform left matrix multiplication"
+    (let* ((a #2A((0 0 1)
+		  (1 0 0)
+		  (0 1 0)))
+	   (b #2A((1 2 3)
+		  (4 5 6)
+		  (7 8 9)))
+	   (ab (matrix* a b))
+	   (c #2A((7 8 9)
+		  (1 2 3)
+		  (4 5 6))))
+      (ok (equalp ab c)))))
