8ffb40e3203e55b7c23c39427ab56cc965066431
erikbackman <erikbackman@users.noreply.github.com>
2023-07-08 19:09:17 +0200

    Add vertex array and layout

--- src/main.lisp
+++ src/main.lisp
@@ -9,26 +9,20 @@
 
     (let* ((shape (make-cube))
 	   (vx-buffer (make-instance 'vx-buffer :data (sd-verts shape)))
-	   (ix-buffer (make-ix-buffer (sd-inds shape)))
+	   (ix-buffer (make-instance 'ix-buffer :data (sd-inds shape)))
+	   (va (make-instance 'vertex-array))
+	   (layout (make-vb-layout))
 	   (shader (with-slots (vs fs) (load-shader "shader.glsl")
 		     (create-shader vs fs))))
 
-      ;; TODO: vertex-array abstraction
-      (let ((stride (* 6 (cffi:foreign-type-size :float))))
-
-	(gl:enable-vertex-attrib-array 0)
-	(gl:vertex-attrib-pointer 0 3 :float nil stride (cffi:null-pointer))
-
-	(gl:enable-vertex-attrib-array 1)
-	(gl:vertex-attrib-pointer 1 3 :float nil stride
-				  (cffi-sys:inc-pointer
-				   (cffi:null-pointer)
-				   (* 3 (cffi:foreign-type-size :float))))
-	(gl:bind-buffer :array-buffer 0)
-	)
+      (vb-layout-push layout 3)
+      (vertex-array-add-buffer va vx-buffer layout)
       (loop until (window-should-close-p)
-	    do (draw vx-buffer ix-buffer shader)
+	    do (draw va ix-buffer shader)
 	       (swap-buffers)
+	       (let ((err (gl:check-error)))
+		 (when err
+		   (print (format nil "GL ERROR: ~a" err))))
 	       (poll-events))
 
       (gl:delete-program shader))))
--- src/render.lisp
+++ src/render.lisp
@@ -1,7 +1,5 @@
 (in-package :untitled)
 
-(defclass vx-buffer () ((id :accessor id)))
-
 (defgeneric buffer-bind (obj))
 (defgeneric buffer-unbind (obj))
 
@@ -12,11 +10,38 @@
     (gl:buffer-data target :static-draw arr)
     (gl:free-gl-array arr)))
 
+;; ========  Vertex buffer ========
+
+(defclass vx-buffer ()
+  ((id :accessor id)))
+
 (defmethod initialize-instance :after ((obj vx-buffer) &key data (size (length data)))
   (with-slots (id) obj
     (setf id (gl:gen-buffer))
     (gl:bind-buffer :array-buffer id)
     (alloc-gl-array data size :array-buffer)))
+
+(defmethod buffer-bind ((obj vx-buffer))
+  (with-slots (id) obj (gl:bind-buffer :array-buffer id)))
+
+(defmethod buffer-unbind ((obj vx-buffer))
+  (gl:bind-buffer :array-buffer 0))
+
+;; ========  Index buffer ========
+
+(defclass ix-buffer ()
+  ((id :accessor id)
+   (cnt :accessor cnt :initform 0)))
+
+(defmethod initialize-instance :after ((obj ix-buffer) &key data (count (length data)))
+  (with-slots (id cnt) obj
+    (setf id (gl:gen-buffer)
+	  cnt count)
+    (gl:bind-buffer :element-array-buffer id)
+    (let ((arr (gl:alloc-gl-array :unsigned-int count)))
+      (dotimes (i count)
+	(setf (gl:glaref arr i) (aref data i))
+	(gl:buffer-data :element-array-buffer :static-draw arr)))))
 
 (defun make-ix-buffer (data &optional (count (length data)))
   (let ((arr (gl:alloc-gl-array :uint count)))
@@ -24,11 +49,56 @@
       (setf (gl:glaref arr i) (aref data i)))
     arr))
 
-(defmethod buffer-bind ((obj vx-buffer))
-  (with-slots (id) obj (gl:bind-buffer :array-buffer id)))
+(defmethod buffer-bind ((obj ix-buffer))
+  (with-slots (id) obj (gl:bind-buffer :element-array-buffer id)))
 
-(defmethod buffer-unbind ((obj vx-buffer))
-  (gl:bind-buffer :array-buffer 0))
+(defmethod buffer-unbind ((obj ix-buffer))
+  (gl:bind-buffer :element-array-buffer 0))
+
+;; ======== Vertex layout ========
+
+(defstruct vb-element type count normalized)
+
+(defstruct vb-layout
+  (elements (make-array '(0) :fill-pointer 0 :adjustable t))
+  (stride 0))
+
+(defun vb-layout-push (layout count)
+  (with-slots (elements stride) layout
+    (vector-push-extend (make-vb-element :type :float :count count :normalized nil) elements)
+    (setf stride (* 6 (cffi:foreign-type-size :float)))))
+
+;; ========  Vertex array ========
+
+(defclass vertex-array ()
+  ((id :initarg :name
+       :accessor id
+       :initform (gl:gen-vertex-array))
+   (elements :initarg :elements :accessor elements :initform nil)))
+
+(defmethod buffer-bind ((obj vertex-array))
+  (with-slots (id) obj (gl:bind-vertex-array id)))
+
+(defmethod buffer-unbind ((obj vertex-array))
+  (gl:bind-vertex-array 0))
+
+(defun vertex-array-add-buffer (va vb layout)
+  (buffer-bind va)
+  (buffer-bind vb)
+  (with-slots (elements stride) layout
+    (loop for i from 0 below (length elements)
+	  for elem = (aref elements i)
+	  do
+	     (gl:enable-vertex-attrib-array i)
+	     (gl:vertex-attrib-pointer 0 3 :float nil stride (cffi:null-pointer))
+
+	     (gl:enable-vertex-attrib-array (+ 1 i))
+	     (gl:vertex-attrib-pointer (+ 1 i) 3 :float nil stride
+				       (cffi-sys:inc-pointer
+					(cffi:null-pointer)
+					(* (slot-value elem 'count)
+					   (cffi:foreign-type-size :float)))))))
+;; ======== Renderer ========
 
 (defparameter *cube-positions*
   #(#( 0.0  0.0  0.0)
@@ -45,14 +115,19 @@
 
 (defun draw (va ib shader)
   (gl:clear :color-buffer-bit :depth-buffer-bit)
-  (buffer-bind va)
   (gl:use-program shader)
-  
   (let* ((fov (camera-fov *camera*))
 	 (projection (mat4-perspective (deg->rad fov) *aspect* 0.1 100.0))
 	 (view (camera-view *camera*)))
+        
     (shader-set-mat4 shader "u_view" view)
     (shader-set-mat4 shader "u_proj" projection)
+
+    (buffer-bind va)
+    (buffer-bind ib)
+
+    (shader-set-mat4 shader "u_model" (matrix* (mat4-translate 0.0 0.0 0.0)))
+
     (loop for pos across *cube-positions*
 	  for i by 1
 	  for angle = (* 20.0 i)
@@ -61,4 +136,5 @@
 		       (mat4-translate (vec-x pos) (vec-y pos) (vec-z pos)))
 	  do
 	     (shader-set-mat4 shader "u_model" model)
-	     (gl:draw-elements :triangles ib))))
+	     (gl:draw-elements :triangles (gl:make-null-gl-array :unsigned-int) :count (slot-value ib 'cnt)))
+    ))
