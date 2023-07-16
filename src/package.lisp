aa1687c3be1e8269c10c66b0d3679e3eb509188d
erikbackman <erikbackman@users.noreply.github.com>
2023-07-16 21:20:17 +0200

    cleanup

--- src/package.lisp
+++ src/package.lisp
@@ -3,10 +3,11 @@
   (error "Use sbcl --dynamic-space-size 4GB"))
 
 (defpackage :untitled
+  (:nicknames :ut)
   (:use :cl :cl-glfw3 :trivial-main-thread)
   (:import-from :cl-opengl)
   (:import-from :alexandria :switch :with-gensyms)
   (:import-from :trivia :match :defpattern :guard1)
-  (:import-from #:gl
-		#:clear)
+  (:import-from :gl :clear)
+  (:import-from :sb-cga :transform-point :translate :vec :matrix*)
   (:shadow :with-window :create-shader))
